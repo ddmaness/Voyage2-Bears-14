f4249360c91a2e9a9147d93a00e9fff5a4e21d35
Donald Maness <ddmaness@gmail.com>
2017-11-04 20:21:22 -0400

    Move fetch function for profile to actions folder

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,2 +1,45 @@
+import 'whatwg-fetch';
+import { decrementProgress, incrementProgress } from './progress';
+
 export const editProfileSuccess = json => ({ type: 'EDIT_PROFILE_SUCCESS', json });
 export const editProfileFailure = error => ({ type: 'EDIT_PROFILE_FAILURE', error });
+
+export function editProfileFunction(userData) {
+    return async(dispatch) => {
+    	//turn on spinner
+    	dispatch(incrementProgress());
+	  
+        // contact the API
+        await fetch(
+        // where to contact
+        'api/profile/edit-profile', 
+        // what to send
+        {
+        	method: 'POST',
+        	body: JSON.stringify(userData),
+        	headers: {
+            'Content-Type': 'application/json',
+        },
+        	credentials: 'same-origin',
+        },
+    	)
+    	.then((response) => {
+        	if (response.status === 200) {
+        		return response.json();
+        	}
+        	return null;
+      		})
+      		.then((json) => {
+        	if (json) {
+            	dispatch(editProfileSuccess(json));
+        	} else {
+            	dispatch(editProfileFailure(new Error('Failed To Update Profile')));
+            }
+        })
+        .catch((error) => {
+        	dispatch(editProfileFailure(new Error(error)));
+        });
+        // turn off spinner
+        dispatch(decrementProgress());
+    }
+}
--- src/components/profile/ProfileEditContainer.jsx
+++ src/components/profile/ProfileEditContainer.jsx
@@ -1,10 +1,8 @@
 import 'whatwg-fetch';
-import { decrementProgress, incrementProgress } from '../../actions/progress';
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { editProfileSuccess, editProfileFailure } from '../../actions/profile';
 
+import { editProfileFunction } from '../../actions/profile';
 import ProfileEdit from './ProfileEdit';
 
 class ProfileEditContainer extends React.Component {
@@ -12,63 +10,20 @@ class ProfileEditContainer extends React.Component {
 		super(props);
 
 		// bound functions
-		this.editProfileFunction = this.editProfileFunction.bind(this);
+		this.editProfile = this.editProfile.bind(this);
 	}
-
-	async editProfileFunction(userData) {
-    	const {
-    		decrementProgressAction,
-    		incrementProgressAction,
-    		editProfileSuccessAction,
-    		editProfileFailureAction,
-    	} = this.props;
-
-    	//turn on spinner
-    	incrementProgressAction();
-	  
-        // contact the API
-        const resp = await fetch(
-        // where to contact
-        'api/profile/edit-profile', 
-        // what to send
-        {
-          method: 'POST',
-          body: JSON.stringify(userData),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'same-origin',
-        },
-      )
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json();
-        }
-        return null;
-      })
-      .then((json) => {
-        if (json) {
-            editProfileSuccessAction(json);
-        } else {
-            editProfileFailureAction(new Error('Failed To Update Profile'));
-            }
-        })
-        .catch((error) => {
-            editProfileFailureAction(new Error(error));
-        });
-  		
-  		console.log(resp);
-
-      // turn off spinner
-      decrementProgressAction();
-    }
+	
+  editProfile(userData) {
+    const { dispatch } = this.props;
+    dispatch(editProfileFunction(userData));
+  }
 
 	render() {
 		return (
 			<ProfileEdit 
 			profile = {this.props.profile} 
 			authentication = {this.props.authentication} 
-			editProfile = {this.editProfileFunction} />
+			editProfile = {this.editProfile} />
 		);
 	}
 }
@@ -80,13 +35,4 @@ function mapStateToProps(state) {
 	};
 }
 
-const mapDispatchToProps = dispatch => {
-	return bindActionCreators({
-		decrementProgressAction: decrementProgress,
-		incrementProgressAction: incrementProgress,
-		editProfileSuccessAction: editProfileSuccess,
-		editProfileFailureAction: editProfileFailure,
-	}, dispatch);
-} 
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileEditContainer);
+export default connect(mapStateToProps)(ProfileEditContainer);
