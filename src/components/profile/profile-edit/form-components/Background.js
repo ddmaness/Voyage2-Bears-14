c8a47af763bbb59426383ea067bd98e428b340fd
asteed21 <asteed21@gmail.com>
2017-11-09 00:15:29 -0500

    fix merge conflict

--- src/components/profile/profile-edit/form-components/Background.js
+++ src/components/profile/profile-edit/form-components/Background.js
@@ -23,23 +23,6 @@ class EditBackground extends React.Component {
 		const { toggleEdit } = this.props;
 		return (
 			<Form>
-<<<<<<< HEAD
-				<FormGroup row className="justify-content-center">
-          <Input
-            id="background"
-            name="background"
-            onChange={this.handleInputChange}
-            onKeyPress={this.handleKeyPress}
-            placeholder="Describe your background"
-            type="textarea"
-            value={this.state.background}
-          />
-        </FormGroup>
-        <FormGroup row className="justify-content-center">
-          <Button outline color="primary" onClick={this.compileFormData}>Save Changes</Button>
-          <Button outline className="ml-3" color="danger" onClick = {toggleEdit}>Cancel</Button>
-        </FormGroup>
-=======
 				<FormGroup row>
 					<Label for="background">Background</Label>
 						<Input
@@ -52,9 +35,8 @@ class EditBackground extends React.Component {
 							value={this.state.background}
 					/>
 				</FormGroup>
-				<Button outline color = "primary" onClick={this.compileFormData}>Save Changes</ Button>
+				<Button outline color = "primary" onClick={this.compileFormData}>Save Changes</Button>
 				<Button className = "cancel" onClick = {toggleEdit}>Cancel</Button>
->>>>>>> bc95dacb61aae8ef8367e5c5f8dcc38ce5593b36
 			</Form>
 		)
 	}
