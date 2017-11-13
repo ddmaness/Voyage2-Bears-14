20f8245813e1d4f9c2a6cb5a5ef6708a5243bd59
Donald Maness <ddmaness@gmail.com>
2017-11-12 19:08:53 -0500

    Add progress bar to profile edit section

---
+++ src/components/profile/profile-edit/ProgressBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Progress } from 'reactstrap';
+import '../ProfilePage.css'
+
+
+class EditBackground extends React.Component {
+  constructor(props) {
+		super(props);
+
+	}
+	
+	calculateProgress = profile => {
+		let progress = 0;
+		if (profile.background !== '') {
+			progress += 25;
+		}
+		if (profile.skills.length !== 0) {
+			progress += 25;
+		}
+		if (profile.languages.length !== 0) {
+			progress += 25;
+		}
+		if (profile.timezone !== '') {
+			progress += 25;
+		}
+		return progress
+	}
+
+  render(){
+		const { userProfile } = this.props;
+		const totalProgress = this.calculateProgress(userProfile);
+		return (
+			<div className = "profile-completion">
+				<p className = "profile-total-completion-info" >Your profile is <span className = "percent-progress">{totalProgress}%</span> complete</p>
+				<Progress color = "info" value = {totalProgress}/>
+				<p className = "profile-completion-info">Please take a moment to complete your profile so that your fellow developers can get to know more about you</p>
+			</div>
+		)
+	}
+}
+
+export default EditBackground;
